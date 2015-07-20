2b3d75975f4e60acb26ec66be9ad91690ac2dc1a
u459154 <u459154@14-0795.ru.nl>
2015-07-20 16:32:49 +0200

    Streamline /update for 'hits'

--- src/java/nl/ru/crpstudio/response/UpdateResponse.java
+++ src/java/nl/ru/crpstudio/response/UpdateResponse.java
@@ -13,24 +13,26 @@ import javax.management.InstanceNotFoundException;
 import javax.management.MBeanException;
 import javax.management.MalformedObjectNameException;
 import javax.management.ReflectionException;
-import nl.ru.util.json.JSONArray;
 import nl.ru.util.json.JSONObject;
 
 /**
  * UpdateResponse
-    Retrieve the results that are requested
-    What the caller gives us is a JSON parameter called "query", containing:
-    * prj     - name of the CRP 
-*     qc      - the number of the QC line
-      sub     - the sub-category (if any)
-      userid  - the userid
-      view    - a number indicating the kind of data we want:
-                1 - per hit
-                2 - per document
-                3 - per group
-                4 - per division
+ *  Retrieve the results that are requested
+ *  What the caller gives us is a JSON parameter called "query", containing:
+ *    prj     - name of the CRP 
+ *    lng     - the language (corpus specification)
+ *    dir     - the sub-directory under the language (more specific corpus specification)
+ *    qc      - the number of the QC line
+ *    sub     - the sub-category (if any)
+ *    userid  - the userid
+ *    view    - a number indicating the kind of data we want:
+ *              1 - per hit
+ *              2 - per document
+ *              3 - per group
+ *              4 - per division
  * 
  * @author Erwin R. Komen
+ * @history 15/jul/2015 Created
  */
 public class UpdateResponse extends BaseResponse {
 
@@ -45,6 +47,8 @@ public class UpdateResponse extends BaseResponse {
       // Collect the JSON from our caller
       oQuery = new JSONObject(request.getParameter("query"));
       if (!oQuery.has("prj")) { sendErrorResponse("UpdateResponse: missing @prj"); return;}
+      if (!oQuery.has("lng")) { sendErrorResponse("UpdateResponse: missing @lng"); return;}
+      if (!oQuery.has("dir")) { sendErrorResponse("UpdateResponse: missing @dir"); return;}
       if (!oQuery.has("qc")) { sendErrorResponse("UpdateResponse: missing @qc"); return;}
       if (!oQuery.has("view")) { sendErrorResponse("UpdateResponse: missing @view"); return;}
       if (!oQuery.has("userid")) { sendErrorResponse("UpdateResponse: missing @userid"); return;}
@@ -52,12 +56,14 @@ public class UpdateResponse extends BaseResponse {
       
       // Start preparing output
       this.params.put("userid", oQuery.getString("userid"));
-      this.params.put("prj", oQuery.getString("prj"));
-      this.params.put("lng", oQuery.getString("lng"));
+      this.params.put("prj", oQuery.getString("prj"));  // Specify the CRP name
+      this.params.put("lng", oQuery.getString("lng"));  // Specify the language
+      this.params.put("dir", oQuery.getString("dir"));  // Specify the part of the language = input corpus
+      // Optional: sub category specification
       if (oQuery.has("sub")) { this.params.put("sub", oQuery.getString("sub")); }
 
       // Start preparing the output of "completeRequest()", which is a mapping object
-      Map<String,Object> output = new HashMap<String,Object>();
+      Map<String,Object> output = new HashMap<>();
       output.put("startTime", this.startTime);
       try {
         output.put("memUsageStart", this.servlet.getCurrentMemUsage());
@@ -67,23 +73,26 @@ public class UpdateResponse extends BaseResponse {
         e1.printStackTrace();
       }
       // Issue the request to the /crpp using the 'query' JSON parameter above
-      String sResp = getCrppResponse("exe", "", this.params);
+      String sResp = getCrppResponse("update", "", this.params);
       if (sResp.isEmpty() || !sResp.startsWith("{")) { sendErrorResponse("UpdateResponse: /crpp does not return JSON"); return;}
       
       // Process the response from /crpp, adding to [output]
+      // ===============================================
+      // TODO: adapt this for the /update response!!!!
       processQueryResponse(sResp, output);
+      // ===============================================
       
       // Send the output to our caller
       sendResponse(output);
     } catch (Exception ex) {
-      sendErrorResponse("ExecuteResponse: could not complete: "+ ex.getMessage());
-      this.displayError("ExecuteResponse error: " + ex.getMessage());
+      sendErrorResponse("UpdateResponse: could not complete: "+ ex.getMessage());
+      this.displayError("UpdateResponse error: " + ex.getMessage());
     }
 	}
 
 	@Override
 	protected void logRequest() {
-		this.servlet.log("ExecuteResponse");
+		this.servlet.log("UpdateResponse");
 	}
 
 	@Override
